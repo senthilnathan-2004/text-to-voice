9de5168c9b20d553d22c5e1ecfb8f1bf0a16d0e9
senthilnathan-2004 <senthilragunathan2004@gmail.com>
2025-03-10 18:42:56 +0530

    first

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Text_voice from './Text_voice'
+
+function App() {
+  
+  return (
+    <>
+     <Text_voice/>
+    </>
+  )
+}
+
+export default App
---
+++ Text_voice.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const TextVoice = () => {
+  //hooks
+  const [text, setText] = useState("");
+  const [lang,setLang] = useState('en-US')
+  const [button,setButton] = useState("Convert to Speech")
+
+  //function
+  const textToSpeech = () => {
+    let isSpeaking = true;
+    const synth = window.speechSynthesis;
+    if (!synth.speaking && text) {
+      const utternace = new SpeechSynthesisUtterance(text);
+      utternace.lang=lang;
+      synth.speak(utternace);
+    }
+    if (text.length > 50) {
+      if (synth.speaking && isSpeaking) {
+         setButton("Pause");
+        synth.resume();
+        isSpeaking = false;
+      } else {
+        setButton("Resume");
+        synth.pause();
+        isSpeaking = true;
+      }
+    } else {
+      isSpeaking = false;
+      setButton("Speaking");
+    }
+    setInterval(() => {
+      if (!synth.speaking && !isSpeaking) {
+        isSpeaking = true;
+        setButton("Convert to Speech");
+      }
+    });
+  };
+  return (
+    <div>
+      <div class="container">
+      <header>Text to Speech Converter</header>
+      <textarea value={text} onChange={(e)=>{setText(e.target.value)}} placeholder="Enter text"></textarea>
+      <button onClick={textToSpeech}>{button}</button>
+      <select onChange={(e)=>{setLang(e.target.value)}}>
+          <option value="en-US">english</option>
+          <option value="ta-IN">tamil</option>
+          <option value="te-IN">telugu</option>
+      </select>
+      <button className="button"  onClick={()=>setText('')}>&#10060;</button>
+    </div>
+    </div>
+  );
+};
+
+export default TextVoice;
